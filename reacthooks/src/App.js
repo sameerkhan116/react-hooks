1d6c33b4740810507e661f9d7ae52f0e5cce7224
Sameer Khan <sameer_khan@student.uml.edu>
2018-10-31 22:07:07 -0400

    Basic todo app with useState

---
+++ reacthooks/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Form from './Form';
+
+export default () => {
+  const [todos, setTodos] = useState([]);
+
+  const toggleComplete = i =>
+    setTodos(
+      todos.map(
+        (todo, k) =>
+          k === i
+            ? {
+                ...todo,
+                complete: !todo.complete,
+              }
+            : todo,
+      ),
+    );
+
+  return (
+    <div>
+      <Form
+        onSubmit={text => setTodos([{ text, complete: false }, ...todos])}
+      />
+      <div>
+        {todos.map(({ text, complete }, i) => (
+          <div
+            key={text}
+            onClick={() => toggleComplete(i)}
+            style={{
+              textDecoration: complete ? 'line-through' : '',
+            }}
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
