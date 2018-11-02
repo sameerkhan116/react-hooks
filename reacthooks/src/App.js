41b61154f7fdae55d6def65e0b3fbd47cdc4e133
Sameer Khan <sameer_khan@student.uml.edu>
2018-11-01 22:08:52 -0400

    Used useEffect

--- reacthooks/src/App.js
+++ reacthooks/src/App.js
@@ -1,8 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Form from './Form';
 
 export default () => {
   const [todos, setTodos] = useState([]);
+  const [person, setPerson] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(async () => {
+    const response = await fetch('https://api.randomuser.me/');
+    const data = await response.json();
+    const [item] = data.results;
+    setPerson(item);
+    setLoading(false);
+  }, []);
 
   const toggleComplete = i =>
     setTodos(
@@ -34,6 +44,11 @@ export default () => {
             {text}
           </div>
         ))}
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          person && <div>{person.name.first}</div>
+        )}
       </div>
     </div>
   );
